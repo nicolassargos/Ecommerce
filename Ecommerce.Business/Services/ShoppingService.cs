20cf1c4855db8c1a7802b7f7fe2be90f30ef0f8d
lamenagere <nicolassargos@hotmail.com>
2019-03-13 13:52:44 +0100

    Ajout des ShoppingCart et ShoppingProduct repository, Models et ShoppingService

---
+++ Ecommerce.Business/Services/ShoppingService.cs
@@ -0,0 +1,30 @@
+﻿using Ecommerce.Business.Models;
+using Ecommerce.Data;
+using Ecommerce.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Business.Services
+{
+    public class ShoppingService
+    {
+        private ShoppingProductRepository _productRepository;
+        private ShoppingCartRepository _cartRepository;
+        private EcommerceContext _context;
+
+        public ShoppingService()
+        {
+            _context = new EcommerceContext();
+            _productRepository = new ShoppingProductRepository(_context);
+            _cartRepository = new ShoppingCartRepository(_context);
+        }
+
+        //public ShoppingCartModel GetShoppingCart(int id)
+        //{
+
+        //}
+    }
+}
---
+++ Ecommerce.Data/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,64 @@
+﻿using Ecommerce.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Data.Repositories
+{
+    public class ShoppingCartRepository : IRepository<ShoppingCart>
+    {
+        private EcommerceContext _context;
+
+        public ShoppingCartRepository(EcommerceContext context)
+        {
+            _context = context;
+        }
+
+        public ShoppingCart Add(ShoppingCart entity)
+        {
+            return _context.ShoppingCarts.Add(entity);
+        }
+
+        public void Delete(ShoppingCart entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<ShoppingCart> GetAll()
+        {
+            return _context.ShoppingCarts;
+        }
+
+        public ShoppingCart GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShoppingCart GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<ShoppingCart> GetByName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShoppingCart GetSingle(Func<ShoppingCart, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShoppingCart Update(ShoppingCart entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
